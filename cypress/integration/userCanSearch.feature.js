87c7f01d7336cb2cd61db44b87ec747c8a973b0e
allezz <aerbay@gmail.com>
2020-05-12 19:48:37 +0200

    Add axios default url to index.js,Add feature test file

---
+++ cypress/integration/userCanSearch.feature.js
@@ -0,0 +1,47 @@
+describe("User can search for actor/movie", () => {
+  before(() => {
+    cy.server();
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/",
+      response: "fixture:search_response.json",
+    });
+    cy.visit("/");
+  });
+  describe("User enter search param and clicks on Search", () => {
+    before(() => {
+      cy.get("input#search").type("Tom Hanks");
+      cy.get("button").contains("Search").click();
+    });
+
+    it("User can see first result", () => {
+      cy.get("#result-item-1").within(() => {
+        cy.contains("Tom Hanks");
+        cy.contains("Cast Away");
+        cy.contains("2000");
+        cy.contains("Producer");
+        cy.find("image").should("be.visible");
+      });
+    });
+
+    it("User can see second result", () => {
+      cy.get("#result-item-2").within(() => {
+        cy.contains("Tom Hand");
+        cy.contains("Scream 2");
+        cy.contains("1997");
+        cy.contains("Actor");
+        cy.find("image").should("be.visible");
+      });
+    });
+
+    it("User can see third result", () => {
+      cy.get("#result-item-3").within(() => {
+        cy.contains("Jim Hanks");
+        cy.contains("Abnormal Attraction");
+        cy.contains("2018");
+        cy.contains("Actor");
+        cy.find("image").should("be.visible");
+      });
+    });
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import axios from "axios";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+axios.defaults.baseURL = "http://localhost:3000/api";
 ReactDOM.render(
   <React.StrictMode>
     <App />
