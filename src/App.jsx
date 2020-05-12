07ba88cebd1c5a9b9e64e8e59a0c97bc51fc3527
allezz <aerbay@gmail.com>
2020-05-12 20:15:56 +0200

    Add onChangeHandler for input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
 
 export class App extends Component {
+  state = {
+    searchText: "",
+  };
+
+  onChangeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
     return (
       <div>
         <h1>Release Tracker</h1>
+
+        <form>
+          <input
+            type="text"
+            id="search"
+            name="searchText"
+            onChange={this.onChangeHandler}
+          />
+          <button type="submit" onClick={this.XXXXX}>
+            Search
+          </button>
+        </form>
       </div>
     );
   }
