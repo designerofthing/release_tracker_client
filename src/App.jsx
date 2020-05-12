8cb3ce114e499b6d0c8a4e897051ca3bea89cdce
allezz <aerbay@gmail.com>
2020-05-12 20:01:43 +0200

    Add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Release Tracker</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
