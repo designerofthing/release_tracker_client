ab96ce5c9ae101d7df330a039e643214fc37b4f6
allezz <aerbay@gmail.com>
2020-05-15 13:40:10 +0200

    Add feature test

---
+++ cypress/integration/userCanSelectGenres.feature.js
@@ -0,0 +1,29 @@
+describe("Visitor can select genres", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("visitor can select and unselect comedy", () => {
+    cy.get("#comedy").should("be.visible");
+    cy.get("#comedy").click();
+    cy.get("#comedy").should("have.class", "active");
+    cy.get("#comedy").click();
+    cy.get("#comedy").should("have.class", "disabled");
+  });
+
+  it("visitor can select and unselect drama", () => {
+    cy.get("#drama").should("be.visible");
+    cy.get("#drama").click();
+    cy.get("#drama").should("have.class", "active");
+    cy.get("#drama").click();
+    cy.get("#drama").should("have.class", "disabled");
+  });
+
+  it("visitor can select and unselect thriller", () => {
+    cy.get("#thriller").should("be.visible");
+    cy.get("#thriller").click();
+    cy.get("#thriller").should("have.class", "active");
+    cy.get("#thriller").click();
+    cy.get("#thriller").should("have.class", "disabled");
+  });
+});
