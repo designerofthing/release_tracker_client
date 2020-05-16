f9c575aa34469c04c5f2eaefc11f6c3241ce30a6
allezz <aerbay@gmail.com>
2020-05-17 01:13:15 +0200

    Add some style

--- src/components/ViewTracker.jsx
+++ src/components/ViewTracker.jsx
@@ -1,12 +1,5 @@
 import React, { Component } from "react";
-import {
-  Container,
-  Image,
-  Table,
-  Segment,
-  Grid,
-  Button,
-} from "semantic-ui-react";
+import { Container, Segment, Grid, Button, List } from "semantic-ui-react";
 
 export class ViewTracker extends Component {
   render() {
@@ -17,29 +10,50 @@ export class ViewTracker extends Component {
 
       return (
         <>
-          <div
+          <List.Item
             id={
               "release-item-" +
               (1 + this.props.trackedInfo.movies.indexOf(result))
             }
           >
-            <h3>{result.title}</h3>
-            <p>{result.release_date}</p>
-            <p>Tracked people: {result.name}</p>
-            <p>{result.role}</p>
-            <p>Tracked genres: {result.genres.join(", ")}</p>
-            <img src={imgPath} height="35" alt={result.title} />
-            <p>{result.description}</p>
-          </div>
+            <List.Icon name="marker" />
+            <List.Content>
+              <List.Header>
+                {result.title}--{result.release_date}
+              </List.Header>
+              <List.Description>
+                <p>{result.description}</p>
+                <b>Role:</b>
+                {result.role} <b>Tracked genres: </b>
+                {result.genres.join(", ")}
+                <p>
+                  <b>Tracked people: </b>
+                  {result.name}
+                </p>
+                <img src={imgPath} height="35" alt={result.title} />
+              </List.Description>
+            </List.Content>
+          </List.Item>
         </>
       );
     });
 
     return (
-      <div>
-        <h1 id="release-header">Upcoming Releases</h1>
-        {releaseTable}
-      </div>
+      <Container>
+        <Grid columns={2} divided fluid relaxed>
+          <Grid.Row>
+            <Grid.Column>
+              <div></div>
+            </Grid.Column>
+            <Grid.Column>
+              <h1 id="release-header">Upcoming Releases</h1>
+              <div style={{ height: "55%", overflow: "scroll" }}>
+                <List>{releaseTable}</List>
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
     );
   }
 }
