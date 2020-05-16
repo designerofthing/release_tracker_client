2229952b403cc040d50f24ac617d75c011267cd8
pauline_barnades <barnades.pauline@gmail.com>
2020-05-16 13:03:21 +0200

    Editing test file

--- cypress/integration/userCanTrackMultiplePeople.feature.js
+++ cypress/integration/userCanTrackMultiplePeople.feature.js
@@ -1,39 +1,5 @@
-describe("User can track multiple people", () => {
-  beforeEach(() => {
-    cy.server();
-    cy.route({
-      method: "GET",
-      url: "http://localhost:3000/api/v1/search*",
-      response: "fixture:search_response.json",
-    });
-    cy.visit("/");
-    cy.get("input#search").type("Tom Hanks");
-    cy.get("button").contains("Search").click();
-  });
 
-  describe("user click on track", () => {
-    beforeEach(() => {
-      cy.server();
-      cy.route({
-        method: "PUT",
-        url: "http://localhost:3000/api/v1/movie_person/*",
-        response: { message: "Person tracked" },
-      });
-      cy.get("#track-1").click();
-      cy.get("#track-2").click();
-    });
-    it("User track first person", () => {
-      cy.get("#track-1").should("contain", "tracked");
-    });
-    it("User can track second person", () => {
-      cy.get("#track-2").should("contain", "tracked");
-    });
-  })
-})
-
-
-
-describe("User can view tracked results", () => {
+describe("User can view upcoming releases from tracked people", () => {
   beforeEach(() => {
     cy.server();
     cy.route({
@@ -45,7 +11,16 @@ describe("User can view tracked results", () => {
     cy.get("button").contains("View Your Tracker").click();
   });
 
-  it("User can see tracked people list", () => {
+  it("list of first tracked person releases", () => {
+    cy.get("#release-item-1").within(() => {
+      cy.contains("Tom Hank's New Drama Movie");
+      cy.contains("Tracked people: Tom Hanks");
+      cy.contains("Tracked genres: Drama, Musical");
+      cy.contains("2023-11-02");
+      cy.contains("Actor");
+      cy.contains("stuff happened");
+      cy.get("img").should("be.visible");
+    });
     cy.get("#release-item-2").within(() => {
       cy.contains("Tom Hank's New Drama Movie");
       cy.contains("Tracked people: Tom Hanks");
@@ -55,6 +30,28 @@ describe("User can view tracked results", () => {
       cy.contains("stuff happened");
       cy.get("img").should("be.visible");
     });
+  });
+
+  it("list of second tracked person releases", () => {
+    cy.get("#release-item-3").within(() => {
+      cy.contains("Will Smith's New Drama Movie");
+      cy.contains("Tracked people: Will Smith");
+      cy.contains("Tracked genres: thriller, Musical");
+      cy.contains("2022-18-02");
+      cy.contains("Actor");
+      cy.contains("crazy stuff happened");
+      cy.get("img").should("be.visible");
+    });
+    cy.get("#release-item-4").within(() => {
+      cy.contains("Will smith's New Comedy Movie");
+      cy.contains("Tracked people: Will Smith");
+      cy.contains("Tracked genres: Comedy, Musical");
+      cy.contains("2021-11-12");
+      cy.contains("Actor");
+      cy.contains("Awesome stuff happened");
+      cy.get("img").should("be.visible");
+    });
+
   });
 
 
